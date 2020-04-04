f0b8034ab77a884f014687eee90e24f8c3b8c2dc
JunHwaPark <a28165415@gmail.com>
2020-04-04 17:16:20 +0900

    fix something

---
+++ Adaptive_Quadrature_c.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <cmath>
+
+long double function(long double x) {
+    return 2 * x * cos(2 * x) - pow(x - 2, 2);
+}
+
+long double simpson(long double start, long double end) {
+    return (end - start) * (function(start) + 4 * function((start + end) / 2) + function(end)) / 6;
+}
+
+long double compute(long double start, long double end) {
+    long double single = simpson(start, end);
+    long double comp = simpson(start, (start + end) / 2) + simpson((start + end) / 2, end);
+
+    if (abs(single - comp) < 0.00001)
+        return comp;
+    else
+        return compute(start, (start + end) / 2) + compute((start + end) / 2, end);
+}
+
+int main()
+{
+    long double value = compute(0, 5);
+    printf("%.15f\n%.15f\tAbsError=%.15f", -15.30630798565174, value, abs(-15.30630798565174 - value));
+}
